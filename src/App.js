6980b865bdc7ce1ba2b93bea1433b36037606a9a
sofien ouni <souni@logiq-digital.com>
2024-06-26 03:25:57 +0100

    Premier commit : ajout du frontend react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [hand, setHand] = useState([]);
+    const [sortedHand, setSortedHand] = useState([]);
+    const [sorted, setSorted] = useState(false); // État pour gérer si la main est triée ou non
+
+    useEffect(() => {
+        fetchHand();
+    }, []);
+
+    const fetchHand = () => {
+        axios.get('http://localhost:8000/main')
+            .then(response => {
+                console.log(response);
+                setHand(response.data);
+            });
+    };
+
+    const sortHand = () => {
+        axios.post('http://localhost:8000/sorted-hand', hand)
+            .then(response => {
+                console.log(response);
+                setSortedHand(response.data);
+                setSorted(true); // Met à jour l'état pour indiquer que la main est triée
+            });
+    };
+
+    return (
+        <div>
+            <h1>Main non triée</h1>
+            <ul>
+                {hand.map((card, index) => (
+                    <li key={index}>{card.color} {card.value}</li>
+                ))}
+            </ul>
+
+            {/* Bouton pour trier la main */}
+            <button onClick={sortHand}>Trier</button>
+
+            {/* Affichage de la main triée si elle est triée */}
+            {sorted && (
+                <>
+                    <h1>Main triée</h1>
+                    <ul>
+                        {sortedHand.map((card, index) => (
+                            <li key={index}>{card.color} {card.value}</li>
+                        ))}
+                    </ul>
+                </>
+            )}
+        </div>
+    );
 }
 
 export default App;
